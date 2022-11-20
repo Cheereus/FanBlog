65ed7348344a2cbf63ee7be80e595b3c1abb0286
Cheereus <fanwei1995@hotmail.com>
2022-11-20 13:59:44 +0000

    初始化服务端

--- server/main.py
+++ server/main.py
@@ -1,7 +1,15 @@
-import BaseHTTPServer
+import http.server as BaseHTTPServer
 
 
-class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class RequestHandler(BaseHTTPServer.SimpleHTTPRequestHandler):
+
+    def __init__(self, *args, directory=None, **kwargs):
+        if directory is None:
+            self.directory = '/home/fanwei/workspace/FanBlog/web/'
+        else:
+            self.directory = directory
+        super().__init__(*args, directory=self.directory, **kwargs)
+
     '''处理请求并返回页面'''
 
     # 页面模板
@@ -13,13 +21,14 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         </html>
     '''
 
-    # 处理一个GET请求
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
-        self.end_headers()
-        self.wfile.write(self.Page)
+    # # 处理一个GET请求
+    # def do_GET(self):
+    #     print(self.path)
+    #     self.send_response(200)
+    #     self.send_header("Content-Type", "text/html")
+    #     self.send_header("Content-Length", str(len(self.Page)))
+    #     self.end_headers()
+    #     self.wfile.write(self.Page.encode())
 
 if __name__ == '__main__':
     serverAddress = ('', 8080)
